5f7e23899f959c40588ccd57b90ba64f6c458844
SHRUTI AGARWAL <56133783+Shruti3004@users.noreply.github.com>
2020-10-12 02:20:52 +0530

    updated port

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ if(process.env.NODE_ENV === 'production'){
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });    
 }
-
-app.listen(5000, () => {
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
     console.log("Listening on Port 5000")
 })
